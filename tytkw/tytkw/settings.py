85bc75244967a1c83505a2ad9eb1f4df59acdf7f
hung222 <90371118+hung222@users.noreply.github.com>
2022-07-17 10:47:39 +0700

    Update settings.py

--- tytkw/tytkw/settings.py
+++ tytkw/tytkw/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i)1f97q51@+3lf)gu&m@z(kj82#dc@kwfl6&us8ijj3vs+*+y_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
